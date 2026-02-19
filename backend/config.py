755161949d83ca5b74fd151b4a8db6d2028358f2
feliciakusuma <147422000+feliciakusuma@users.noreply.github.com>
2026-02-19 15:53:05 +0700

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -45,7 +45,7 @@ MS_REDIRECT_URI = os.getenv(
 # Email (verification)
 EMAIL_SENDER = os.getenv("EMAIL_SENDER", "election.noreply@gmail.com")
 SMTP_HOST = os.getenv("SMTP_HOST", "smtp.gmail.com")
-SMTP_PORT = int(os.getenv("SMTP_PORT", "587"))
+SMTP_PORT = int(os.getenv("SMTP_PORT", "2525"))
 SMTP_USERNAME = os.getenv("SMTP_USERNAME", EMAIL_SENDER)
 SMTP_PASSWORD = os.getenv("SMTP_PASSWORD","miud xzce erzg pcca")
 SMTP_USE_TLS = os.getenv("SMTP_USE_TLS", "true").lower() == "true"
